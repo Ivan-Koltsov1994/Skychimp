3dc161a075a155ed1879e1a309f5a989b4b994ec
Ivan <ivan.koltsov.1994@mail.com>
2023-06-29 00:01:28 +0500

    перезалил проект посе ошибки БД

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
-from blog.models import Post
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    list_filter = ('name',)
-    search_fields = ('name',)
+# Register your models here.
