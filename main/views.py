ba9a9bddcae45f75897bfbf80697b23fbe2e6eac
Ivan <ivan.koltsov.1994@mail.com>
2023-06-29 20:53:15 +0500

    correct project

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from blog.models import Post
 from main.forms import ClientForms, MessageForms
 from main.models import Sending, Clients, Message, Attempt
 from main.services import send_email
@@ -17,7 +18,13 @@ class IndexView(TemplateView):
         'object_list': Sending.objects.all()
     }
 
-class ClientListView(LoginRequiredMixin,ListView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['last_posts'] = Post.objects.all().order_by('?')[:3]
+        return context
+
+
+class ClientListView(LoginRequiredMixin, ListView):
     model = Clients
     extra_context = {
         'object_list': Clients.objects.all(),
@@ -25,8 +32,8 @@ class ClientListView(LoginRequiredMixin,ListView):
     }
 
     def get_queryset(self):
-        queryset= super().get_queryset()
-        queryset = queryset.filter(is_active = True)
+        queryset = super().get_queryset()
+        queryset = queryset.filter(is_active=True)
         if self.request.user.has_perm('main.can_view_client'):
             return queryset
         return Clients.objects.filter(created_by=self.request.user)
@@ -38,10 +45,12 @@ class ClientDetailView(DetailView):
         'title': 'Детали клиента'
     }
 
-class ClientCreateView(LoginRequiredMixin,CreateView):
+
+class ClientCreateView(LoginRequiredMixin, CreateView):
     model = Clients
     form_class = ClientForms
     success_url = reverse_lazy('main:clients_list')
+
 
 class ClientUpdateView(UpdateView):
     model = Clients
@@ -55,7 +64,8 @@ class ClientDeleteView(DeleteView):
     model = Clients
     success_url = reverse_lazy('main:clients_list')
 
-class MessageListView(LoginRequiredMixin,ListView):
+
+class MessageListView(LoginRequiredMixin, ListView):
     model = Message
     extra_context = {
         'message_list': Message.objects.all(),
@@ -67,6 +77,7 @@ class MessageDetailView(DetailView):
     model = Message
     success_url = reverse_lazy('main:message_list')
 
+
 class MessageCreateView(CreateView):
     model = Message
     form_class = MessageForms
@@ -75,7 +86,7 @@ class MessageCreateView(CreateView):
 
 class MessageUpdateView(UpdateView):
     model = Message
-    fields = ('subject', 'body', )
+    fields = ('subject', 'body',)
 
     def get_success_url(self):
         return reverse('main:message_view', args=[str(self.object.pk)])
@@ -86,7 +97,7 @@ class MessageDeleteView(DeleteView):
     success_url = reverse_lazy('main:message_list')
 
 
-class SendingListView(LoginRequiredMixin,ListView):
+class SendingListView(LoginRequiredMixin, ListView):
     model = Sending
     extra_context = {
         'object_list': Sending.objects.all(),
@@ -100,14 +111,14 @@ class SendingListView(LoginRequiredMixin,ListView):
         return Sending.objects.filter(created=self.request.user)
 
 
-
 class SendingDetailView(DetailView):
     model = Sending
 
     def test_func(self):
         return self.request.user.has_perm(perm='main.set_sending_status')
 
-class SendingCreateView(LoginRequiredMixin,CreateView):
+
+class SendingCreateView(LoginRequiredMixin, CreateView):
     model = Sending
     fields = ('message', 'frequency', 'status', 'created')
     success_url = reverse_lazy('main:sending_list')
@@ -116,7 +127,7 @@ class SendingCreateView(LoginRequiredMixin,CreateView):
 
 class SendingUpdateView(UpdateView):
     model = Sending
-    fields = ('message', 'frequency', 'status', )
+    fields = ('message', 'frequency', 'status',)
 
     def get_success_url(self):
         return reverse('main:sending_view', args=[str(self.object.pk)])
@@ -127,7 +138,7 @@ class SendingDeleteView(DeleteView):
     success_url = reverse_lazy('main:sending_list')
 
 
-class AttemptListView(LoginRequiredMixin,ListView):
+class AttemptListView(LoginRequiredMixin, ListView):
     model = Attempt
     extra_context = {
         'object_list': Attempt.objects.all(),
@@ -142,7 +153,6 @@ class AttemptDetailView(DetailView):
     }
 
 
-
 def set_is_active(request, pk):
     clients_item = get_object_or_404(Clients, pk=pk)
     if clients_item.is_active:
@@ -151,6 +161,7 @@ def set_is_active(request, pk):
         clients_item.is_active = True
     clients_item.save()
     return redirect(reverse('main:clients_list'))
+
 
 def set_status_sending(request, pk):
     sending_item = get_object_or_404(Sending, pk=pk)
@@ -161,6 +172,7 @@ def set_status_sending(request, pk):
         sending_item.status = Sending.CREATED
         sending_item.save()
     return redirect(reverse('main:sending_list'))
+
 
 class SearchResultsView(ListView):
     model = Clients
